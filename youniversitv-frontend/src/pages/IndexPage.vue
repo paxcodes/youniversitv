74c1815d022e23f9be1518b5df76b3f2c96d49b0
Pax <13646646+paxcodes@users.noreply.github.com>
2023-11-25 16:59:34 -0800

    feat: Embed a video

--- youniversitv-frontend/src/pages/IndexPage.vue
+++ youniversitv-frontend/src/pages/IndexPage.vue
@@ -1,49 +1,29 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <iframe
+      width="560"
+      height="315"
+      :src="mainVideo"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+  setup() {
+    const videos = ref<string[]>(['BlSVX1WqTXk', 'vkM_2vsHWA4']);
+    const mainVideo = ref<string>(
+      `https://youtube.com/embed/${videos.value[0]}`
+    );
+    return { videos, mainVideo };
+  },
+  methods: {},
 });
 </script>
